5770ccc49aaae5e5b1e5dfdd596058fe5a538ad6
Peter Lay <peterlaywaugh@gmail.com>
2023-08-28 16:53:43 +0200

    Added user_id reference to notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_140536) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_145130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_140536) do
     t.bigint "notifiable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "requests", force: :cascade do |t|
