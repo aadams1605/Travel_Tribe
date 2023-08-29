b2729c7a6de2b0a2d45169d6312cef98109f1e0c
claudiaamprimo <claudia.amprimo@gmail.com>
2023-08-29 12:04:37 +0200

    strong params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,3 +6,13 @@ class EventsController < ApplicationController
   def show
   end
 end
+
+private
+
+def set_event
+  @event = Event.find(params[:id])
+end
+
+def event_params
+  params.require(:event).permit(:title, :description, :location, :start_date, :end_date, :capacity)
+end
