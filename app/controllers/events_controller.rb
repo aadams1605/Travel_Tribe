3b124cbd4da315af2516806e77df9bce686071d9
Peter Lay <peterlaywaugh@gmail.com>
2023-08-29 11:41:40 +0200

    Added update and destroy actions to Events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+
+  def update
+    if @listing.update(event_params)
+      redirect_to @event, notice: "Event was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.delete
+    redirect_to root_path notice: "Event was successfully deleted."
+  end
 end
