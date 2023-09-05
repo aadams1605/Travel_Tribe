36bebf5c6fd5ddbd8ce30bbbd4039ba0f23f10c3
claudiaamprimo <claudia.amprimo@gmail.com>
2023-09-05 16:24:05 +0200

    add new notification icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :clear_notifications
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :description, :location, :gender, :age, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :description, :location, :gender, :age, :photo])
+  end
+
+  def clear_notifications
+    previous_page = Rails.application.routes.recognize_path(request.referrer)
+    return unless previous_page[:controller] == "notifications" && previous_page[:action] == "index"
+
+    current_user.notifications.update_all(read: true)
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
+  after_action :clear_notifications
 
   def index
-
   end
+
 end
