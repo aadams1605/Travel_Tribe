5f5303d2a68ba93e879473303b057b33676543cb
Peter Lay <peterlaywaugh@gmail.com>
2023-09-06 15:52:47 +0200

    Fixed bug in event delete notification

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :requests, dependent: :destroy
   has_one :chatroom, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
-  has_many :notifications, :as => :notifiable
+  has_many :notifications, :as => :notifiable, dependent: :destroy
   validates :title, :description, :location, :capacity, :start_date, :end_date, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
